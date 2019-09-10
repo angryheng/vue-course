43824043986c5c919ab772fa360448f5c1547d6a
zxh <angryheng@gmail.com>
2019-09-10 15:19:38 +0800

    learn router-por

---
+++ src/views/Error_404.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="error_404">
+    <h1>Sorry, The page not found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
